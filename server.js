66590cadd68e60a7b39e9f16e2b4ef7710028c16
Way-Endless <74210287+Way-Endless@users.noreply.github.com>
2021-05-09 13:21:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,32 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://admin:192837465@endlessway.en3q2.mongodb.net/endlessway?retryWrites=true&w=majority",
+{
+useUnifiedTopology: true,
+useNewUrlParser: true,
+useFindAndModify: false
+}
+)
+.then(console.log("MONGODB CONNECTED!"));
+
+const notesSchema = {
+title: String,
+content: String
+}
+
+const Note = mongoose.model("Note", notesSchema);
+
+app.get("/", function(req, res) {
+res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res) {
+let newNote = new Note({
+title: req.body.title,
+content: req.body.content
+});
+newNote.save();
+res.redirect('/');
+})
